94cc605a6029be3d5ba37bdd30c1335226851f25
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-12-28 00:45:27 +0100

    Add simple quicksort

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,4 +3,11 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn $ show $ quicksort [10, 2, 5, 3, 1, 6, 7, 4, 2, 3, 4, 8, 9]
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort (filter (<=x) xs)
+        biggerSorted = quicksort (filter (>x) xs)
+    in smallerSorted ++ [x] ++ biggerSorted
