7f1d3ec2ca9330c0bbc92792af09c08a157825d2
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-12-28 01:12:47 +0100

    Add weird maths function

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,7 +3,7 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn $ show $ quicksort [10, 2, 5, 3, 1, 6, 7, 4, 2, 3, 4, 8, 9]
+someFunc = putStrLn $ show $ howManyElementsDoesItTakeForTheSumOfTheRootsOfAllNaturalNumbersToExceedANumber 1000
 
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
@@ -11,3 +11,6 @@ quicksort (x:xs) =
     let smallerSorted = quicksort (filter (<=x) xs)
         biggerSorted = quicksort (filter (>x) xs)
     in smallerSorted ++ [x] ++ biggerSorted
+
+howManyElementsDoesItTakeForTheSumOfTheRootsOfAllNaturalNumbersToExceedANumber :: (Floating a, Ord a, Enum a) => a -> Int
+howManyElementsDoesItTakeForTheSumOfTheRootsOfAllNaturalNumbersToExceedANumber x = (+) 1 $ length $ takeWhile (<x) $ scanl1 (+) $ map sqrt [1..]
