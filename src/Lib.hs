f740c4d1e3c61ca6412e52afda7d8b783979cd4d
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-12-28 21:12:24 +0100

    Add fancy fns

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,8 +2,13 @@ module Lib
     ( someFunc
     ) where
 
+import Data.List
+import Data.Function
+import Data.Char
+
+
 someFunc :: IO ()
-someFunc = putStrLn $ show $ makeAllNegative [1, 2, 2 ,3 ,4]
+someFunc = putStrLn $ show $ caesarEncode 4 "Hi there!"
 
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
@@ -20,3 +25,15 @@ makeAllNegative = map $ negate . abs
 
 boomBangs :: (Integral a) => [a] -> [String]
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x] 
+
+countUnique :: (Eq a) => [a] -> Int
+countUnique = length . nub
+
+groupByLength :: (Num a) => [[a]] -> [[a]]
+groupByLength = sortBy (compare `on` length)
+
+caesarEncode :: Int -> String -> String
+caesarEncode shift msg =
+    let ords = map ord msg
+        shifted = map (+ shift) ords
+    in map chr shifted
